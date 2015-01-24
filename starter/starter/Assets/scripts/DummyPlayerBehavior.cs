d5d2b8f612e04df4426ce9c4e229c95ab6dd025a
Francis Joseph Serina <francis.serina@gmail.com>
2015-01-24 11:21:54 -0800

    + Added player/room interaction. See RoomTest scene

---
+++ starter/starter/Assets/scripts/DummyPlayerBehavior.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DummyPlayerBehavior : MonoBehaviour {
+
+	public int sparePartsOnHand = 2;
+	private Transform room;
+	
+	void EnteredRoom ( Transform enteredRoom ) {
+		room = enteredRoom;
+	}
+	
+	void LeftRoom () {
+		room = null;
+	}
+	
+	void DismantleSuccess () {
+		++sparePartsOnHand;
+		Debug.Log ("I acquired a spare part");
+	}
+	
+	void DismantleFailed () {
+		Debug.Log ("Room has no more spare parts");
+	}
+	
+	void FixSuccess () {
+		--sparePartsOnHand;
+		Debug.Log("Placed a spare part");
+	}
+	
+	void FixFailed () {
+		Debug.Log ("Room has enough parts to work");
+	}
+	
+	void Update () {
+		if (room) {
+			if (Input.GetKeyDown(KeyCode.Q)) {
+				// Salvage
+				room.SendMessage("Dismantle");
+			} else if (Input.GetKeyDown(KeyCode.E)) {
+				if (sparePartsOnHand > 0) {
+					// Power Up
+					room.SendMessage("Fix");
+				} else {
+					Debug.Log ("I don't have enough spare parts");
+				}
+			}
+		}
+	}
+}
---
+++ starter/starter/Assets/scripts/RoomBehavior.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomBehavior : MonoBehaviour {
+
+	public string roomName;
+	public int currentSpareParts = 0;
+	public int neededSpareParts = 4;
+	
+	private Transform player;
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log (roomName + " room currently has " + currentSpareParts + " out of the " + neededSpareParts + " spare parts to be active.");
+		
+		// Player object should receive this
+		other.SendMessage("EnteredRoom", transform);
+		player = other.transform;
+	}
+	
+	void OnTriggerExit(Collider other) {
+		Debug.Log ("You left the " + roomName + " room");
+	
+		other.SendMessage("LeftRoom");
+		player = null;
+	}
+	
+	// Player object should call this when dismantling
+	void Dismantle () {
+		if (currentSpareParts > 0) {
+			--currentSpareParts;
+			player.SendMessage("DismantleSuccess");
+			Debug.Log(currentSpareParts + " / " + neededSpareParts);
+		} else {
+			player.SendMessage("DismantleFailed");
+		}
+	}
+	
+	// Player object should call this when putting in a spare part
+	void Fix () {
+		if (currentSpareParts < neededSpareParts) {
+			++currentSpareParts;
+			player.SendMessage("FixSuccess");
+			Debug.Log(currentSpareParts + " / " + neededSpareParts);
+		} else {
+			player.SendMessage("FixFailed");
+		}
+	}
+}
