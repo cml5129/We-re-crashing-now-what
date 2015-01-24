4f3c48fb198dedced7ea0a7845cf10ea8812efd5
Francis Joseph Seriña <francis.serina@digipen.edu>
2015-01-24 15:18:20 -0800

    + Level1 + minor ambient lighting

---
+++ starter/starter/Assets/scripts/FlickeringLight.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Light)) ]
+
+public class FlickeringLight : MonoBehaviour {
+		
+	private float maxLightIntensity;
+	private Light lightComponent;
+
+	IEnumerator Start () {
+		lightComponent = GetComponent<Light>();
+		maxLightIntensity = lightComponent.intensity;
+
+		while (true) {
+			lightComponent.intensity = 0;
+			yield return new WaitForSeconds(Random.Range(0.5f, 1.5f));
+
+			int count = Random.Range (1,5);
+			while (count > 0) {
+				lightComponent.intensity = Random.Range (0.2f, maxLightIntensity);
+				yield return new WaitForSeconds(Random.Range (0.01f,0.3f) );
+
+				lightComponent.intensity = 0;
+				yield return new WaitForSeconds(Random.Range (0.05f,0.3f) );
+				--count;
+			}
+		}
+	}
+}
---
+++ starter/starter/Assets/scripts/RotateAlongX.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateAlongX : MonoBehaviour {
+
+	public float rotSpeed;
+
+	void Update () {
+		transform.Rotate(rotSpeed * Time.deltaTime,0,0);
+	}
+}
