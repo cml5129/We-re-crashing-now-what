51a5fd1f837e28256781a7e47d39775ff560dfcc
Francis Joseph Seriña <francis.serina@digipen.edu>
2015-01-25 15:55:00 -0800

    - Removed Shadows from all lights ~ Updated Grov location. Grov head always looks at player

---
+++ starter/starter/Assets/scripts/LookAtPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtPlayer : MonoBehaviour {
+
+	private Quaternion startRot;
+	public Transform player;
+
+	void Start () {
+		startRot = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion desiredRot = Quaternion.LookRotation(player.position - transform.position);
+		transform.rotation = Quaternion.Euler(0, 90, 0) * desiredRot; //startRot * desiredRot;
+	}
+}
